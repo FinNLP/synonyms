032ed2f7f64fddac03b77de664dc55073d625c20
Alex Corvi <alex@arrayy.com>
2017-01-01 23:54:58 +0300

    lower case the input

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const dictionary = require('./dictionary.js');
 const synonyms = function(word,type){
-	var entry = dictionary[word] || deep(word,type); // doesn't exist
+	word = word.toLowerCase();
+	type = type.toLowerCase();
+	var entry = dictionary[word] || deep(word); // doesn't exist
 	if(!entry) return undefined;
 	else if(type) return entry[type];
 	else return entry;
 };
 
-function deep(wordR,typeR){
+function deep(wordR){
 	var synonyms = {};
 	for(var entry in dictionary){
 		for(var type in dictionary[entry]){
